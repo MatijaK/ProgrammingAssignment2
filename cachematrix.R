1c088029ea964a2e94291312227a2f90ac22f28f
MatijaK <MatijaK@users.noreply.github.com>
2014-12-17 22:15:12 +0100

    Update cachematrix.R
    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -2,31 +2,49 @@
 
 test_M <- function(n){
      # n <- 5
-      v_mx <- sample(1:(10*n), n^2, replace = TRUE)
+      v_mx <- sample(1:(10*n), n^2, replace = TRUE) #creates a randome database
       bm <- matrix(v_mx, nrow=n, ncol=n)
-      x <- bm
+
+      # before calling, assign to variable
       
 }
-
-### Assignment
       
 makeCacheMatrix <- function(x=matrix()){
       
-#Creating a list of matrix value and the inverse belonging to the matrix
+#Creating a list of functions
+      #* Matrix + Inverse
+      #* Matrix
+      #* Calculated Inverse
+      #* Value of calculated inverse
       
+      #These functions arrange the logic of the cachesolve programe so that it 
+      #reduces the need to write variables in the general memory. It basically
+      # creates the function's internal RAM with a list
+
+      
+      # The first part just setts the value of Inverse to 0 - empty
       Invr <- NULL
+      
       set <- function(y){
             
             x <<- y
             Invr<<- NULL
             
       }
+      #The set function will assign the inverse to the vector as the 1st element
       
+      #Indicating the matrix at hand to be used in the cachesolve function      
       get <- function(){x}
-      setInvr <- function(solve) {Invr <<- solve}
-      getInvr <- function() {Invr}
-      list(set =set, get=get, setInvr= setInvr, getInvr=getInvr)
       
+      #This functionwill set the inverse when calculated
+      setInvr <- function(solve) {Invr <<- solve}
+      
+      #It basically just says that you will get the inverse when calling the f.
+      getInvr <- function() {Invr}
+      
+      list(set =set, get=get, setInvr= setInvr, getInvr=getInvr)
+      # we name the elements by their variable names so that we can refer to 
+      # them in cachesolve with $
             
 }
       
@@ -35,17 +53,21 @@ makeCacheMatrix <- function(x=matrix()){
 cachesolve <- function(x, ...) {
       
       Invr <- x$getInvr()
+      #It looks for the written value of Inverse if the x matches to the Matrix
+      #for which the inverse was already calculated
+      
       if(!is.null(Invr)){
             message("getting cached data")
             return(Invr)
       }
-      
-      data <- x$get()
-      Invr <- solve(data, ...)
-      x$setInvr(Invr)
+      #We make sure that we indicate thatwe have found it +display it directly
+
+
+      data <- x$get()   #It looks for the matrix
+      Invr <- solve(data, ...)      #It calculates the inverse
+      x$setInvr(Invr)   #it sets an inverse
   
       # Printing the solution
-      
       Invr  
       
 }
