50a5ee0a0f7049d98affa1871ca5e2317c4bd5cc
MatijaK <MatijaK@users.noreply.github.com>
2014-12-17 21:30:22 +0100

    Update cachematrix.R
    My solution to the 2nd programing assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+###Test matrix that can be used to test the program
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+test_M <- function(n){
+     # n <- 5
+      v_mx <- sample(1:(10*n), n^2, replace = TRUE)
+      bm <- matrix(v_mx, nrow=n, ncol=n)
+      x <- bm
+      
 }
+      
+makeCacheMatrix <- function(x=matrix()){
+      
+#Creating a list of matrix value and the inverse belonging to the matrix
+      
+      Invr <- NULL
+      set <- function(y){
+            
+            x <<- y
+            Invr<<- NULL
+            
+      }
+      
+      get <- function(){x}
+      setInvr <- function(solve) {Invr <<- solve}
+      getInvr <- function() {Invr}
+      list(set =set, get=get, setInvr= setInvr, getInvr=getInvr)
+      
+            
+}
+      
+# The function that actually looks for the solution
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+      
+      Invr <- x$getInvr()
+      if(!is.null(Invr)){
+            message("getting cached data")
+            return(Invr)
+      }
+      
+      data <- x$get()
+      Invr <- solve(data, ...)
+      x$setInvr(Invr)
+  
+      # Printing the solution
+      
+      Invr  
+      
 }
